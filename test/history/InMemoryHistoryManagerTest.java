e578fcc770f5aca0ffe21cbdf786cbb49a95e367
Nikita Rovnin <Nikita.Rovnin@gmail.com>
2025-06-19 20:28:41 +0700

    Добавить тест на добавление Добавить тест на удаление Добавить тест на перезаписывание Добавить тест на удаление несуществующей задачи Добавить тест на удаление единственной задачи

--- test/history/InMemoryHistoryManagerTest.java
+++ test/history/InMemoryHistoryManagerTest.java
@@ -10,6 +10,9 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayDeque;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class InMemoryHistoryManagerTest {
     @Test
     void add() {
@@ -17,7 +20,7 @@ class InMemoryHistoryManagerTest {
         Task testTask1 = taskManager.createTask("TestTask1Name", "TestTask1Description");
         Task testTask1Answer = taskManager.getTaskById(testTask1.getId());
 
-        Assertions.assertEquals(testTask1, testTask1Answer, "Задача не создалась.");
+        assertEquals(testTask1, testTask1Answer, "Задача не создалась.");
     }
 
     @Test
@@ -35,5 +38,79 @@ class InMemoryHistoryManagerTest {
         ));
 
         Assertions.assertIterableEquals(tasks, historyManager.getHistory(), "Возвращен не тот список задач, что ожидался.");
+    }
+
+    @Test
+    void addTasksAndCheck() {
+        HistoryManager historyManager = Managers.getDefaultHistory();
+
+        Task testTask1 = new Task(1, "TestTask1Name", "TestTask1Description");
+        historyManager.add(testTask1);
+        Task testTask2 = new Task(2, "TestTask2Name", "TestTask2Description");
+        historyManager.add(testTask2);
+        Task testTask3 = new Task(3, "TestTask3Name", "TestTask3Description");
+        historyManager.add(testTask3);
+
+        assertEquals(List.of(testTask1, testTask2, testTask3), historyManager.getHistory());
+    }
+
+    @Test
+    void addTasksAndRemove() {
+        HistoryManager historyManager = Managers.getDefaultHistory();
+
+        Task testTask1 = new Task(1, "TestTask1Name", "TestTask1Description");
+        historyManager.add(testTask1);
+        Task testTask2 = new Task(2, "TestTask2Name", "TestTask2Description");
+        historyManager.add(testTask2);
+        Task testTask3 = new Task(3, "TestTask3Name", "TestTask3Description");
+        historyManager.add(testTask3);
+
+        historyManager.remove(1);
+
+        List<Task> history = historyManager.getHistory();
+        assertEquals(2, history.size());
+        assertEquals(2, history.getFirst().getId());
+    }
+
+    @Test
+    void notAddDuplicateTaskInHistory() {
+        HistoryManager historyManager = Managers.getDefaultHistory();
+
+        Task testTask1 = new Task(1, "TestTask1Name", "TestTask1Description");
+        historyManager.add(testTask1);
+        Task testTask2 = new Task(2, "TestTask2Name", "TestTask2Description");
+        historyManager.add(testTask2);
+        Task testTask3 = new Task(3, "TestTask3Name", "TestTask3Description");
+        historyManager.add(testTask3);
+        Task testTask2Duplicate = new Task(2, "TestTask2DuplicateName", "TestTask2DuplicateDescription");
+        historyManager.add(testTask2Duplicate);
+
+        List<Task> history = historyManager.getHistory();
+        assertEquals(3, history.size());
+        assertEquals(testTask2Duplicate, history.get(2));
+    }
+
+    @Test
+    void doNothingIfRemovingNotExistingTask() {
+        HistoryManager historyManager = Managers.getDefaultHistory();
+
+        Task testTask1 = new Task(1, "TestTask1Name", "TestTask1Description");
+        historyManager.add(testTask1);
+
+        historyManager.remove(2);
+
+        assertEquals(1, historyManager.getHistory().size());
+    }
+
+    @Test
+    void deleteLastTask() {
+        HistoryManager historyManager = Managers.getDefaultHistory();
+
+        Task testTask1 = new Task(1, "TestTask1Name", "TestTask1Description");
+        historyManager.add(testTask1);
+
+        historyManager.remove(1);
+
+        assertTrue(historyManager.getHistory().isEmpty());
     }
 }
