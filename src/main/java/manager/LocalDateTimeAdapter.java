5e015795ed7df1d1dbae20f98ea34de0d35207e1
Nikita Rovnin <Nikita.Rovnin@gmail.com>
2025-08-04 14:17:38 +0700

    Правка импортов, адаптеров

--- src/main/java/manager/LocalDateTimeAdapter.java
+++ src/main/java/manager/LocalDateTimeAdapter.java
@@ -2,7 +2,6 @@ package main.java.manager;
 
 import com.google.gson.*;
 import java.lang.reflect.Type;
-import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -18,18 +17,5 @@ class LocalDateTimeAdapter implements JsonSerializer<LocalDateTime>, JsonDeseria
     public LocalDateTime deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
             throws JsonParseException {
         return LocalDateTime.parse(json.getAsString(), formatter);
-    }
-}
-
-class DurationAdapter implements JsonSerializer<Duration>, JsonDeserializer<Duration> {
-    @Override
-    public JsonElement serialize(Duration src, Type typeOfSrc, JsonSerializationContext context) {
-        return new JsonPrimitive(src.toString()); // например "PT1H30M"
-    }
-
-    @Override
-    public Duration deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
-            throws JsonParseException {
-        return Duration.parse(json.getAsString());
     }
 }
