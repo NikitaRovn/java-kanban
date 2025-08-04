db107214f9c576f0214295838c599d6bfbc549b2
Nikita Rovnin <Nikita.Rovnin@gmail.com>
2025-08-04 14:23:48 +0700

    Починить тесты

--- test/manager/HttpTaskServerTasksTest.java
+++ test/manager/HttpTaskServerTasksTest.java
@@ -1,16 +1,20 @@
 package manager;
 
+import com.google.gson.reflect.TypeToken;
 import main.java.manager.*;
 import main.java.tasks.Task;
+import main.java.tasks.TaskDto;
 import org.junit.jupiter.api.*;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.HashMap;
 
+import static main.java.manager.HttpTaskServer.gson;
 import static org.junit.jupiter.api.Assertions.*;
 
 class HttpTaskServerTasksTest {
@@ -33,19 +37,32 @@ class HttpTaskServerTasksTest {
 
     @Test
     void testAddTask() throws IOException, InterruptedException {
-        Task task = new Task(0, "Test task", "Test description");
+        TaskDto dto = new TaskDto(null, "Test task", "Test description", null);
 
-        String json = HttpTaskServer.gson.toJson(task);
+        String json = gson.toJson(dto);
 
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://localhost:8080/tasks")).POST(HttpRequest.BodyPublishers.ofString(json)).build();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8080/tasks"))
+                .POST(HttpRequest.BodyPublishers.ofString(json))
+                .build();
 
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
         assertEquals(201, response.statusCode());
 
-        HashMap<Integer, Task> tasks = manager.getAllTasks();
-        assertEquals(0, tasks.size());
-        assertEquals("Test task", task.getName());
+        HttpRequest getRequest = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8080/tasks"))
+                .GET()
+                .build();
+
+        HttpResponse<String> getResponse = client.send(getRequest, HttpResponse.BodyHandlers.ofString());
+
+        Type type = new TypeToken<HashMap<Integer, Task>>() {}.getType();
+        HashMap<Integer, Task> tasks = gson.fromJson(getResponse.body(), type);
+
+        assertEquals(1, tasks.size());
+        Task addedTask = tasks.values().iterator().next();
+        assertEquals("Test task", addedTask.getName());
     }
 }
